274f7f6f170ff463a943ff3aa40943b5c1d68705
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-05-30 18:43:07 +0700

    membuat menu

--- cirquear/cirquear.cpp
+++ cirquear/cirquear.cpp
@@ -109,6 +109,29 @@ int main() {
 	char ch;
 
 	while (true) {
+		cout << "Menu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Implement delete operation" << endl;
+		cout << "3. Display queue" << endl;
+		cout << "4. Exit" << endl;
+		cout << "Pilihan: ";
+		cin >> ch;
+		cout << endl;
 
+		switch (ch) {
+		case '1':
+			q.insert(0);
+			break;
+		case '2':
+			q.remove();
+			break;
+		case '3':
+			q.display();
+			break;
+		case '4':
+			exit(1);
+		default:
+			cout << "Pilihan tidak valid" << endl;
+		}
 	}
 }
